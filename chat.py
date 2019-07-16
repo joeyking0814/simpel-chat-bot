46c0fb88daf29e689ca52642680e6d92d9d0c2fd
joeyking0814 <52970029+joeyking0814@users.noreply.github.com>
2019-07-16 14:03:14 -0500

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,11 @@
+import random
+import time
+print("loding ∆'")
+greet =("hello", "hi", "good day")
+ greetR print("olleh", "ih", "yad doog")
+joke =("tell me a joke")
+print("efil ruoy", "uoy")
+are you= ("are you part of the illuminati")
+print("yes")
+by("by")
+print("by")
